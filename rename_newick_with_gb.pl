59ecef41f0487e26ef1a10aef9df7c0d9410c9d1
Thomas J Creedy <thomas.creedy@gmail.com>
2019-09-20 20:25:42 -0300

    Initial commit

---
+++ rename_newick_with_gb.pl
@@ -0,0 +1,137 @@
+#!/usr/bin/env perl
+#By Thomas J Creedy, thomas@tjcreedy.co.uk 
+
+use warnings;
+use strict;
+use Data::Dumper;
+use Getopt::Long;
+use Bio::SeqIO;
+use Bio::TreeIO;
+use File::Basename;
+
+my @gbpaths;
+my $treepath;
+my $outpath;
+
+my %fieldmethods = ('LOCUS'		=> 'display_id',
+		    'DEFINITION'	=> 'description',
+		    'ACCESSION'		=> 'accession_number',
+		    'VERSION'		=> 'version',
+		    'LENGTH'		=> 'length');
+my @fieldstrings;
+my $taxitems;
+my $replacespace;
+my $help;
+
+my $script = basename($0, ());
+
+########################################################
+# USAGE
+#
+my $usage =<<USAGE;
+
+Description:
+	This script renames the terminals of newick-format trees using data from a GenBank-format file by matching the current names of the tree against the LOCUS field of the genbank file.
+	
+	Multiple GenBank-format files can be passed to the -genbank argument. Any entries in the GenBank file not found in the tree(s) are silently ignored.
+	
+	Multiple trees can be renamed, provided they are provided as multiple lines in a single file passed to -tree
+	
+	Specify one or more of the following fields in a space separated list to the -fields argument:
+		LOCUS
+		DEFINITION
+		ACCESSION
+		VERSION
+		LENGTH
+		ORGANISM
+		TAXONOMY
+	
+	The TAXONOMY field corresponds to the semicolon-separated string given immediately after the ORGANISM field in a GenBank-format file, plus the ORGANISM. The number of taxonomic levels to be returned can be controlled using the -taxitems option, starting at the lowest taxonomic level. Thus the use of -f ORGANISM and -f TAXONOMY -taxitems 1 is exactly identical.
+	
+	The selected fields will be concatenated with hypens and surround by single quotation marks. Optionally, spaces can be replaced with underscores using the -replacespace option.
+	
+	Any tree terminals not in the supplied GenBank-format files will be silently ignored and unchanged.
+	
+Usage:
+
+	perl $script -genbank <in.gb> [<in2.gb>] -tree <tree.tre> -out <out.tre> -fieldstring FIELD [FIELD] [-taxitems n] [-replacespace]
+
+Arguments:
+
+	       genbank:  The path to one or more genbank files from which metadata will be extracted
+	          tree:  The path to a text file containing one or more trees
+	           out:  The path to write the renamed trees
+	        fields:  A space-separated list of fields to use for renaming
+	      taxitems:  If TAXONOMY is specified, the number of taxonomic levels to include
+	  replacespace:  Replace spaces in new names with underscores
+	          help:  Prints out this helpful message
+
+USAGE
+#
+######################################################
+
+
+GetOptions("genbank=s{1,}"	=> \@gbpaths,
+	   "tree=s"		=> \$treepath,
+	   "out=s"		=> \$outpath,
+	   "fieldstring=s{1,}"	=> \@fieldstrings,
+	   "taxitems=i"		=> \$taxitems,
+	   "replacespace"	=> \$replacespace,
+	   "help"		=> \$help) or die "\nError getting options\n\n";
+
+print $usage and exit if $help;
+
+my %conversion;
+
+foreach my $gbpath (@gbpaths){
+	my $gb_in = Bio::SeqIO->new(-file => $gbpath,
+				    -format => "genbank");
+	
+	while(my $seq = $gb_in->next_seq){
+		
+		my $seqname = $seq->display_id();
+		my @fieldarray;
+		
+		die "Error: $seqname in $gbpath is a duplicate of a previous entry!\n" if $conversion{$seqname};
+		
+		foreach my $f (@fieldstrings){
+			my $outitem;
+			if($fieldmethods{$f}){
+				my $method = $fieldmethods{$f};
+				$outitem = $seq->$method;
+			} elsif($f eq "ORGANISM"){
+				$outitem = $seq->species->node_name();
+			} elsif($f eq "TAXONOMY"){
+				my @taxa = $seq->species->classification();
+				$taxitems = scalar @taxa if(!$taxitems or $taxitems > scalar @taxa);
+				$outitem =  join(";", reverse(@taxa[0..$taxitems-1]));
+			}
+			$outitem =~ s/\.$//;
+			push @fieldarray, $outitem;
+		}
+		
+		my $outline = join("-", @fieldarray);
+		$outline =~ s/ /_/g if $replacespace;
+		$outline = "\'$outline\'";
+		$conversion{$seqname} = $outline;
+	}
+}
+
+open my $treein, '<', $treepath or die "Error opening $treepath\n";
+open my $treeout, '>', $outpath or die "Error opening $outpath for writing\n";
+
+while(my $tree = <$treein>){
+	chomp $tree;
+	foreach my $seqname (keys %conversion){
+		if($tree =~ /$seqname/){
+			print "Renaming $seqname -> $conversion{$seqname}\n";
+			$tree =~ s/$seqname/$conversion{$seqname}/;
+		}
+	}
+	print $treeout "$tree\n";
+}
+
+close $treein;
+close $treeout;
+
+exit;
