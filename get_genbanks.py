59c8b499e6aa2b8c6a6dc9ba55ccfb86adf02b19
Thomas J. Creedy <thomas@tjcreedy.co.uk>
2020-05-08 19:38:49 +0100

    Added STDERR progress reports

--- get_genbanks.py
+++ get_genbanks.py
@@ -16,7 +16,7 @@ n = 0
 for chunk in chunker(acclist, chunk):
     start = chunk * n + 1
     sys.stderr.write("getting genbanks chunk %s-%s\n" % 
-                     (start, start + len(chunk)))
+                     (start, start + len(chunk) - 1))
     accstring = ','.join(chunk)
     url = ("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?"
           + "db=nuccore&id=" + accstring + "&rettype=gb")
